8019e121f10aeb6e7ebced1c3f98fc13f4890e53
ahmedomarbenhazem <benhazem.ahmedomar@gmail.com>
2026-04-16 22:11:20 +0100

    merged projects into main

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:ahmedahmed12345@localhost/ppp")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:mypassword@localhost:5432/indusdb")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
